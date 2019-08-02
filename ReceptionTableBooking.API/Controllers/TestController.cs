1b08d14b2c53ad8be927339fb85e1dccde247e19
adarshmsaryan <36766914+adarshmsaryan@users.noreply.github.com>
2019-08-02 19:41:18 +0530

    Add files via upload

---
+++ ReceptionTableBooking.API/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+
+namespace ReceptionTableBooking.API.Controllers
+{
+    public class TestController : ApiController
+    {
+        public class user
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+        public string GetMeValue()
+        {
+            return "Value";
+        }
+        public string valid(user u)
+        {
+            if(u.username=="adarsh" && u.password=="123")
+            {
+                return "succes";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+    
+    }
+}
